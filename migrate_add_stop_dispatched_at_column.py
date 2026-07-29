baf7b13f426a1eb1083b1419221b880f40beadd4
林彥妤 <stephanieyenyu@gmail.com>
2026-07-29 13:34:21 +0800

    Delete migrate_add_stop_dispatched_at_column.py

--- migrate_add_stop_dispatched_at_column.py
+++
@@ -1,6 +0,0 @@
-from app.db import engine
-from sqlalchemy import text
-
-with engine.begin() as conn:
-    conn.execute(text("ALTER TABLE packages ADD COLUMN IF NOT EXISTS package_count INTEGER NOT NULL DEFAULT 1"))
-print("欄位新增完成")
