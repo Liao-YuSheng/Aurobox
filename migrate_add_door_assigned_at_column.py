3c9f611fd90b1d5060e886b7248ae8dd1b75ad93
林彥妤 <stephanieyenyu@gmail.com>
2026-07-29 13:33:23 +0800

    Delete migrate_add_door_assigned_at_column.py

--- migrate_add_door_assigned_at_column.py
+++
@@ -1,6 +0,0 @@
-from app.db import engine
-from sqlalchemy import text
-
-with engine.begin() as conn:
-    conn.execute(text("ALTER TABLE packages ADD COLUMN IF NOT EXISTS door_assigned_at TIMESTAMP"))
-print("欄位新增完成")
