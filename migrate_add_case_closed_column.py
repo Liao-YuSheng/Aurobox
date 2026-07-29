ede76f12e0221db0ae1cabb56416b0d31e1c5282
林彥妤 <stephanieyenyu@gmail.com>
2026-07-29 13:33:13 +0800

    Delete migrate_add_case_closed_column.py

--- migrate_add_case_closed_column.py
+++
@@ -1,6 +0,0 @@
-from app.db import engine
-from sqlalchemy import text
-
-with engine.begin() as conn:
-    conn.execute(text("ALTER TABLE packages ADD COLUMN IF NOT EXISTS case_closed_at TIMESTAMP"))
-print("欄位新增完成")
